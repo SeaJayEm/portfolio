772b1fbdfc0e6b24ab5c3cf33b97adce45f0e523
Claire Mercier <47384001+SeaJayEm@users.noreply.github.com>
2025-02-04 10:15:48 +0100

    Update streamlit_app.py
    adjust top margin

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,12 @@ st.markdown(
         .stApp {
             background-image: url("https://images.unsplash.com/photo-1519751138087-5bf79df62d5b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
             background-size: cover;
+            padding-top: 0px !important;
+            margin-top: -50px !important;  /* Ajuste selon besoin */
+        }
+        .block-container {
+        padding-top: 0px !important;
+        margin-top: -50px !important; /* Ajuste cette valeur si nécessaire */
         }
         html, body, [class*="css"], h1, h2, h3, h4, h5, h6, p, li, span, div {
             color: #2e2e36 !important;
